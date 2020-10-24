9bb99a11146d0e9398b1b475055f54442b7f4cb4
Mast_mex <mastmex42@gmail.com>
2020-10-24 15:48:09 +0300

    lesson 8

--- src/lesson7/One.java
+++ src/lesson7/One.java
@@ -3,11 +3,15 @@ package lesson7;
 public class One {
     String text ="Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     int sum=0;
+    int c=0;
     public One(){
         String[] words= text.split("\\s");
        for(String i:words) {
            try {
                sum += Integer.parseInt(i);
+               if(c==1)
+                   sum-=Integer.parseInt(i);
+               c++;
            } catch (NumberFormatException e) {
                sum += 0;
            }
