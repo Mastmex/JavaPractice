2f4071f7840026acf7459d0aa8f0123b99f3d3a0
Mastmex <mastmex42@gmail.com>
2020-10-30 20:14:15 +0300

    lesson8

--- src/lesson8/Test.java
+++ src/lesson8/Test.java
@@ -3,8 +3,8 @@ package lesson8;
 public class Test  {
 
     public static void main(String[] args) {
-        //One o = new One();
-        //Two t = new Two();
+        One o = new One();
+        Two t = new Two();
         Three8 tr = new Three8();
     }
 }
